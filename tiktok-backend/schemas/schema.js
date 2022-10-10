4c67612de9f2532444e7f0b296f14b1f4b630b99
susan56789 <mwihaki2susan@gmail.com>
2022-10-10 04:31:41 +0300

    Updated schems

--- tiktok-backend/schemas/schema.js
+++ tiktok-backend/schemas/schema.js
@@ -5,7 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import user from './user'
 import post from './post'
-import comment from './comment'
+import comments from './comments'
 import postedBy from './postedBy'
 
 // Then we give our schema to the builder and provide the result to Sanity
@@ -18,7 +18,7 @@ export default createSchema({
     /* Your types here! */
     user,
     post,
-    comment,
+    comments,
     postedBy
   ]),
 })
