f2354e8d8370a843fc35634b95deb2562619dd02
susan56789 <mwihaki2susan@gmail.com>
2022-10-24 06:14:55 +0300

    Initial Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 
 const Home: NextPage = () => {
   return (
     <div>
-      Tiktok
 
+      <Head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Tiktok</title>
+      </Head>
+
+      Tiktok
     </div>
   )
 }
