6f4bc3d86a1a131001df035f240760b38ecec2b2
susan56789 <mwihaki2susan@gmail.com>
2022-10-26 07:24:45 +0300

    intial commit

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -15,7 +15,7 @@ const Discover = () => {
      cursor-pointer justfy-center`
 
     const topicStyles = `xl:border-2
-     hover:bg-primary text-black
+     hover:bg-primary hover:text-black
      xl:border-gray-300 px-3 py-2 rounded
       xl:rounded-full flex items-center gap-2
       cursor-pointer justfy-center`
