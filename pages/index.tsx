5e48f634700400afc5bab03075a22fa45d5a1993
susan56789 <mwihaki2susan@gmail.com>
2022-10-26 07:13:54 +0300

    Initial Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home = ({ videos }: IProps) => {
   // console.log(videos)
   return (
     <div>
-
+    
       <Head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
